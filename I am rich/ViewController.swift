55ae0c4e7f813d0a1c78667955957a56d4b184d9
pawan6000 <59465844+pawan6000@users.noreply.github.com>
2020-09-11 10:03:22 +0530

    Add files via upload

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Pawan Awar on 07/09/20.
+//  Copyright © 2020 pawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
